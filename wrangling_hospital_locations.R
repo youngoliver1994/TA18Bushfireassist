d694943fa6869f746a52f36a5e4a636495e3c511
Rochelle <rysed1@student.monash.edu>
2020-09-17 21:26:13 +0530

    Create wrangling_hospital_locations.R

---
+++ wrangling_hospital_locations.R
@@ -0,0 +1,56 @@
+# load library
+library(dplyr)
+
+# read in dataset
+hospital_locations <- read.csv('Hospital_Locations.csv')
+
+# nullify/drop columns
+hospital_locations[,c(3,5,10,11,13,15,16,17)] <- list(NULL)
+
+# change column names
+colnames(hospital_locations)[1] <- "Longitude"
+colnames(hospital_locations)[2] <- "Latitude"
+colnames(hospital_locations)[3] <- "MedicalCentre"
+colnames(hospital_locations)[8] <- "Area"
+
+# concatenate the Street Number, Road Name and Road Type to one column
+hospital_locations$Address <- paste(hospital_locations$StreetNum, hospital_locations$RoadName)
+hospital_locations$Address <- paste(hospital_locations$Address, hospital_locations$RoadType)
+
+# nullify the concatenated columns
+hospital_locations[,c(5:7)] <- list(NULL)
+
+# change order of Latitude and Longitude for convenience
+hospital_locations <- hospital_locations[,c(2,1,3,4,7,5,6)]
+
+# create vector of specialisations
+specialisations <- c('Surgery','Surgicentre','Plastic Surgery',
+                     'Specialist','Procedure','Endoscopy','Scope','Eye','Ear',
+                     'Dialysis','IVF','SurgiCentre','Day','Rehabilitation',
+                     'Digestive','Cosmetic','Laser','Gastroenterology',
+                     'Vision','Imaging','Oncology','Radiation','Oral',
+                     'Facial','Gut','MediBrain','MediSleep','Cancer','Cosmetic',
+                     'Dental','Hospice','Marie Stopes','Mandometer',
+                     'Surgical','Womens','Women')
+
+# remove rows which contain the specialisation in the Medical Centre name
+for (specialisation in specialisations){
+  hospital_locations <- hospital_locations[!grepl(specialisation, 
+                                                 hospital_locations$`MedicalCentre`),]
+}
+
+# reset index
+rownames(hospital_locations) <- NULL
+
+# change Area column case to upper case
+hospital_locations$Area <- toupper(hospital_locations$Area)
+
+# remove values within parentheses in Area and Medical Centre column
+hospital_locations$Area <- gsub("\\s*\\([^\\)]+\\)","",as.character(hospital_locations$Area))
+hospital_locations$`MedicalCentre` <- gsub("\\s*\\([^\\)]+\\)","",as.character(hospital_locations$`MedicalCentre`))
+
+# remove commas and everything after the comma in Medical Centre column
+hospital_locations$`MedicalCentre` <- gsub("(.*),.*", "\\1", as.character(hospital_locations$`MedicalCentre`))
+
+# write to file
+write.csv(hospital_locations, 'Medical Centres.csv')
